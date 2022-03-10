e54e413f67eb04466a8266c5b3fe639f3d8bbae2
Dattaguru Bhat <dattaguru.bhat@comviva.com>
2022-03-10 10:43:07 +0530

    updated

--- basics/playground.js
+++ basics/playground.js
@@ -1,2 +1,23 @@
-console.log(String.fromCharCode(97))
-console.log('Aa'.charCodeAt())
+function myFunc(x, y, ...params) {
+    console.log(x);
+    console.log(y);
+    console.log(params)
+}
+
+myFunc("a", "b", "c", "d", "e", "f")
+// "a"
+// "b"
+// ["c", "d", "e", "f"]
+
+myFunc('a', 'b', { obj: 1, obj2: 2 })
+myFunc('a', 'b', { obj: 1, obj2: 2 })
+
+
+const {x, y, ...z} = {x: 1, y: 2, a: 3, b: 4};
+console.log(x); // 1
+console.log(y); // 2
+console.log(z); // { a: 3, b: 4 }
+
+const n = {x, y, ...z};
+console.log(n); // { x: 1, y: 2, a: 3, b: 4 }
+
